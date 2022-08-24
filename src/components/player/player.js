d3c8b8197645beaa3e6627b0ab99e792227f38e3
Andreas Lillje <al225wg@student.lnu.se>
2022-08-24 16:12:52 +0200

    Add play/pause function to player

--- src/components/player/player.js
+++ src/components/player/player.js
@@ -1,14 +1,15 @@
 import "./player.css";
 import * as React from "react";
-import { useEffect } from "react"
+import { useState, useRef, useEffect } from "react";
 import Playbutton from "./img/play-button-2.png";
-import Sound from "./sounds/test-sound.wav";
+import PauseButton from "./img/pause-button.png";
+import sound from "./sounds/test-sound.wav";
 import audioController from "../../services/audio-controller";
-import { useSelector, useDispatch } from 'react-redux'
-import { startPlaying } from '../../redux/reducers/audio'
-import { stopPlaying } from '../../redux/reducers/audio'
-import store from '../../redux/store'
-
+import { useSelector, useDispatch } from "react-redux";
+import { startPlaying } from "../../redux/reducers/audio";
+import { stopPlaying } from "../../redux/reducers/audio";
+import store from "../../redux/store";
+import { AdUnitsOutlined } from "@mui/icons-material";
 
 /**
  * Topbar Component.
@@ -17,34 +18,43 @@ import store from '../../redux/store'
  * @returns {React.ReactElement} - Sidebar Component.
  */
 const Player = ({ noise }) => {
-    const isPlaying = useSelector((state) => state.audio.isPlaying)
-    const dispatch = useDispatch()
+  const [paused, setPaused] = useState(true);
 
-  const toggleNoise = () => {
-    console.log(isPlaying)
-    const audio = new Audio(Sound);
+  const isPlaying = useSelector((state) => state.audio.isPlaying);
+  const dispatch = useDispatch();
+  const audio = useRef(new Audio(sound));
 
-    if (isPlaying) {
-    store.dispatch(stopPlaying())
-        audio.pause()
-    } else {
-        store.dispatch(startPlaying())
-        audio.play()
+  const play = () => {
+    store.dispatch(startPlaying());
 
-    }
+    setPaused(false);
+    audio.current.play();
+  };
+
+  const pause = () => {
+    setPaused(true);
+    store.dispatch(stopPlaying());
+
+    audio.current.pause();
+    audio.current.currentTime = 0;
   };
 
   useEffect(() => {
-  }, [isPlaying])
+    // Loop the sound when it has ended
+    audio.current.addEventListener('ended', function() {
+        audio.current.currentTime = 0;
+        audio.current.play()
+    })
+  }, []);
 
   return (
     <div className="playerWrapper">
       <div className="playButtonWrapper">
         <img
           className="playButtonImg"
-          src={Playbutton}
-          alt="Play button"
-          onClick={toggleNoise}
+          src={!paused ? PauseButton : Playbutton}
+          alt="Pause button"
+          onClick={paused ? play : pause}
         ></img>
       </div>
       <div className="textContent">{noise}</div>
--- src/services/audio-controller.js
+++ src/services/audio-controller.js
@@ -1,17 +1,21 @@
 import { startPlaying } from '../redux/reducers/audio'
 import { stopPlaying } from '../redux/reducers/audio'
+import store from '../redux/store'
 
 
 
 const audioController = (audio) => {
 
-    
-//   if (audio.paused) {
 
-//     audio.play();
-//   } else {
-//     audio.pause();
-//   }
+  if (audio.paused) {
+    store.dispatch(startPlaying())
+    audio.play();
+  } else {
+    audio.muted = true
+    audio.pause();
+    store.dispatch(stopPlaying())
+
+  }
 
 };
 
