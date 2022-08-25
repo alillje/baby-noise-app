362cceca52eef9cb2b9f9337577fdb1ec52c6444
Andreas Lillje <al225wg@student.lnu.se>
2022-08-25 14:13:47 +0200

    Add new sounds

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import './App.css';
 import Layout from './layout/layout/layout'
 import Player from './components/player/player'
-import whiteNoise from './sounds/white-noise.wav'
-import slowRain from './sounds/slow-rain.wav'
-import heavyRain from './sounds/heavy-rain.wav'
-import oceanWaves from './sounds/ocean-waves.wav'
-import acFan from './sounds/ac-fan.wav'
-
+import whiteNoise from './sounds/white-noise.mp3'
+import brownNoise from './sounds/brown-noise.mp3'
+import pinkNoise from './sounds/pink-noise.mp3'
+import wind from './sounds/wind.mp3'
+import rainAndThunder from './sounds/rain-and-thunder.mp3'
+import oceanWaves from './sounds/ocean-waves.mp3'
+import winterWind from './sounds/winter-wind.mp3'
+import arcticWind from './sounds/arctic-wind.mp3'
 
 function App() {
   return (
     <Layout>
-      <Player nameOfSound="White Noise" audioFile={whiteNoise} />
-      <Player nameOfSound="Slow Rain" audioFile={slowRain}/>
-      <Player nameOfSound="Heavy Rain" audioFile={heavyRain} />
-      <Player nameOfSound="Ocean Waves" audioFile={oceanWaves} />
-      <Player nameOfSound="AC Fan" audioFile={acFan} />
+      <Player nameOfSound="White Noise" audioFile={whiteNoise}/>
+      <Player nameOfSound="Brown Noise" audioFile={brownNoise} />
+      <Player nameOfSound="Pink Noise" audioFile={pinkNoise}/>
+      <Player nameOfSound="Wind" audioFile={wind}/>
+      <Player nameOfSound="Rain and Thunder" audioFile={rainAndThunder}/>
+      <Player nameOfSound="Ocean Waves" audioFile={oceanWaves}/>
+      <Player nameOfSound="Winter Wind" audioFile={winterWind}/>
+      <Player nameOfSound="Arctic Wind" audioFile={arcticWind}/>
+
+
+
 
     </Layout>
   );
